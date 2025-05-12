53ee04115760fd2aee21dca7369549bba6268fe6
asselyam <assel.massaurova@gmail.com>
2025-05-12 17:10:42 +0200

    Update playback.py

--- edge-device/src/sound/playback.py
+++ edge-device/src/sound/playback.py
@@ -1,26 +1,12 @@
-import RPi.GPIO as GPIO
+# sound_service.py
 import subprocess
-import time
 
-# Set up button on Gpio 16
-BUTTON_PIN = 16
+class SoundPlayer:
+    def __init__(self, sound_file):
+        self.sound_file = sound_file
+    
+    def play(self):
+        subprocess.run(["aplay", self.sound_file], check=True)
 
-#path to wav
-MUSIC_FILE = "/home/admin/Downloads/rpi-telegram-doorbell-main/edge-device/src/sound/doorbell.wav"
-
-#initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-print("Waiting for button press...")
-
-try:
-        while True:
-                if GPIO.input(BUTTON_PIN) == GPIO.LOW:
-                        print("Button pressed! Play doorbell sound...")
-                        subprocess.run(["aplay", MUSIC_FILE], check=True)
-                        time.sleep(0.3)
-                time.sleep(0.1)
-except KeyboardInterrupt:
-        GPIO.cleanup()
-        print("\nExitting...")
+# Initialize with your sound file
+player = SoundPlayer("/home/admin/Downloads/rpi-telegram-doorbell-main/edge-device/src/sound/doorbell.wav")
